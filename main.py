aa112fadf76cbf9fd1df5d9913b553004a462589
annabelo <annabelowen7@gmail.com>
2026-06-29 16:53:46 +0100

    Add first room representation

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 print("Emergent Misalignment MAS Simulator")
 print("Version 1: baby command-line simulation")
+
+room = {
+    "name": "Room A",
+    "is_available": True,
+    "is_comfortable": True,
+}
+
+print(room)
