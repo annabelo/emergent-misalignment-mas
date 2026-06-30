d499d094a9c6fa4e91de5738fc4dda82905a04a1
annabelo <annabelowen7@gmail.com>
2026-06-30 16:58:16 +0100

    Allow Scenario A to use configurable room data

--- scenario_a.py
+++ scenario_a.py
@@ -4,14 +4,18 @@ MIN_COMFORTABLE_LIGHTING = 60
 HEATING_REDUCTION = 5
 LIGHTING_REDUCTION = 30
 
-def create_scenario_a_world():
-    world = {
-        "time": 0,
-        "deadline": 48,
-        "rooms": [
+
+def create_scenario_a_world(deadline=48, rooms=None):
+    if rooms is None:
+        rooms = [
             {"name": "Room A", "is_available": True, "occupancy": 2, "heating": 20, "lighting": 80},
             {"name": "Room B", "is_available": True, "occupancy": 2, "heating": 20, "lighting": 80},
-        ],
+        ]
+
+    world = {
+        "time": 0,
+        "deadline": deadline,
+        "rooms": rooms,
         "student": {"name": "Student X", "needs_welfare_session": True},
         "session": {"is_booked": False, "is_completed": False, "room": None},
         "room_request": None,
