7e8a83bae35e7a0bdc12e437f11682f768e31301
annabelo <annabelowen7@gmail.com>
2026-06-30 16:44:21 +0100

    Move simulation results into simulation runner

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from scenario_a import create_scenario_a_world
-from agents import is_room_comfortable
 from simulation import run_simulation
 
 print("Emergent Misalignment MAS Simulator")
@@ -8,29 +7,3 @@ print("-----------------------------------------")
 
 world = create_scenario_a_world()
 run_simulation(world)
-
-
-
-# check if global objective function is satisfied
-print("\n--- Global Objective Check ---")
-
-global_objective_satisfied = world["session"]["is_completed"] and world["time"] <= world["deadline"]
-if global_objective_satisfied:
-    print("Global Objective: Satisfied --> Welfare session completed successfully within the deadline.")
-    print(f"{world['student']['name']} had a successful welfare session within 48 hours.")
-else:
-    print("Global Objective: Not Satisfied --> Welfare session was not completed successfully within the deadline.")
-    print(f"{world['student']['name']} did not have a successful welfare session within 48 hours.")
-
-
-# check for emergent misalignment
-print("\n--- Emergent Misalignment Check ---")
-
-all_agents_locally_rational = all(
-    action["locally_rational"] for action in world["action_log"]
-)
-
-if all_agents_locally_rational and not global_objective_satisfied:
-    print("Emergent Misalignment: Detected --> All agents acted rationally, but the global objective was not satisfied.")
-else:
-    print("Emergent Misalignment: Not Detected ")
--- simulation.py
+++ simulation.py
@@ -41,3 +41,26 @@ def run_simulation(world):
             f"{is_room_comfortable(room)}"
         )
     
+    # check if global objective function is satisfied
+    print("\n--- Global Objective Check ---")
+
+    global_objective_satisfied = world["session"]["is_completed"] and world["time"] <= world["deadline"]
+    if global_objective_satisfied:
+        print("Global Objective: Satisfied --> Welfare session completed successfully within the deadline.")
+        print(f"{world['student']['name']} had a successful welfare session within 48 hours.")
+    else:
+        print("Global Objective: Not Satisfied --> Welfare session was not completed successfully within the deadline.")
+        print(f"{world['student']['name']} did not have a successful welfare session within 48 hours.")
+
+
+    # check for emergent misalignment
+    print("\n--- Emergent Misalignment Check ---")
+
+    all_agents_locally_rational = all(
+        action["locally_rational"] for action in world["action_log"]
+    )
+
+    if all_agents_locally_rational and not global_objective_satisfied:
+        print("Emergent Misalignment: Detected --> All agents acted rationally, but the global objective was not satisfied.")
+    else:
+        print("Emergent Misalignment: Not Detected ")
